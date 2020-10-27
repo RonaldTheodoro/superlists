3fe2c3e6cd341a6834c0229ce50806b9e75dff3b
Ronald Theodoro <ronaldtheodoro@gmail.com>
2020-10-27 20:18:05 -0300

    Refactor index view to use a template

--- apps/lists/tests.py
+++ apps/lists/tests.py
@@ -1,20 +1,13 @@
-from django.urls import resolve
-from django.test import TestCase
 from django.http import HttpRequest
+from django.template.loader import render_to_string
+from django.test import TestCase
+from django.urls import resolve
 
 from apps.lists import views
 
 
 class HomePageTest(TestCase):
 
-    def test_root_url_resolves_to_home_page(self):
-        found = resolve('/')
-        self.assertEqual(found.func, views.index)
-
     def test_index_return_correct_html(self):
-        request = HttpRequest()
-        response = views.index(request)
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'index.html')
--- apps/lists/views.py
+++ apps/lists/views.py
@@ -1,8 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
-
 def index(request):
-    return HttpResponse('<html><title>To-Do lists</title></html>')
-
+    return render(request, 'index.html')
