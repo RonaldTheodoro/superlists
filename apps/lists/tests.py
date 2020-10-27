699bf8fb578bec0c685217a6f178185a35e0c294
Ronald Theodoro <ronaldtheodoro@gmail.com>
2020-10-26 22:36:27 -0300

    First unit test anu url mapping, dummy view

--- apps/lists/tests.py
+++ apps/lists/tests.py
@@ -1,7 +1,11 @@
+from django.urls import resolve
 from django.test import TestCase
 
+from apps.lists import views
 
-class SmokeTest(TestCase):
 
-    def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page(self):
+        found = resolve('/')
+        self.assertEqual(found.func, views.index)
--- config/urls.py
+++ config/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from apps import lists
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', lists.views.index, name='index'),
 ]
